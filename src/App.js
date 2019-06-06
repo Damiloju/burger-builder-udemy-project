273fa301bb1ac1c2d3e9fb4297eefcfa15bf7230
Yusuf Damiloju <--add>
2019-06-06 16:36:38 +0100

    general app improvements: add lazy loading and enable redux devtools only in development mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, lazy, Suspense } from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
+import Spinner from "./components/UI/Spinner/Spinner";
 import * as actions from "./store/actions/index";
+
+const Auth = lazy(() => import("./containers/Auth/Auth"));
+const Checkout = lazy(() => import("./containers/Checkout/Checkout"));
+const Orders = lazy(() => import("./containers/Orders/Orders"));
 
 class App extends Component {
 	componentDidMount() {
@@ -29,6 +31,7 @@ class App extends Component {
 					<Route path="/checkout" component={Checkout} />
 					<Route path="/orders" component={Orders} />
 					<Route path="/logout" component={Logout} />
+					<Route path="/auth" component={Auth} />
 					<Route path="/" component={BurgerBuilder} />
 					<Redirect to="/" />
 				</Switch>
@@ -36,7 +39,9 @@ class App extends Component {
 		}
 		return (
 			<div className="App">
-				<Layout>{routes}</Layout>
+				<Layout>
+					<Suspense fallback={<Spinner />}>{routes}</Suspense>
+				</Layout>
 			</div>
 		);
 	}
