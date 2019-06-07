0b742a1ce2f872934f158689ccf61ccc24ec59fc
Yusuf Damiloju <--add>
2019-06-07 16:18:24 +0100

    Updates

---
+++ precache-manifest.700137672aa7ace1fe09249bfe58c05b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1e54273db2a76739b77",
+    "url": "/burger-builder-udemy-project/static/css/4.a3630bff.chunk.css"
+  },
+  {
+    "revision": "6ed6d5c6dd66a9be03e2",
+    "url": "/burger-builder-udemy-project/static/css/main.7d0b1236.chunk.css"
+  },
+  {
+    "revision": "4da1a8b4af71ae2b125f",
+    "url": "/burger-builder-udemy-project/static/js/runtime~main.6accc8fa.js"
+  },
+  {
+    "revision": "d3525a0d655753402a4f",
+    "url": "/burger-builder-udemy-project/static/js/2.2334fa2d.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder-udemy-project/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "4af6e2f5edefc5bb4f6d",
+    "url": "/burger-builder-udemy-project/static/js/3.ecca7ac9.chunk.js"
+  },
+  {
+    "revision": "6ed6d5c6dd66a9be03e2",
+    "url": "/burger-builder-udemy-project/static/js/main.2e4efeeb.chunk.js"
+  },
+  {
+    "revision": "a1e54273db2a76739b77",
+    "url": "/burger-builder-udemy-project/static/js/4.86ede610.chunk.js"
+  },
+  {
+    "revision": "60a86cd7f96f40abe983",
+    "url": "/burger-builder-udemy-project/static/js/5.83483a8a.chunk.js"
+  },
+  {
+    "revision": "60a86cd7f96f40abe983",
+    "url": "/burger-builder-udemy-project/static/css/5.bffab08a.chunk.css"
+  },
+  {
+    "revision": "4af6e2f5edefc5bb4f6d",
+    "url": "/burger-builder-udemy-project/static/css/3.d9d7c2a6.chunk.css"
+  },
+  {
+    "revision": "0d15b43c6efba55bcf15f5d83f53babd",
+    "url": "/burger-builder-udemy-project/index.html"
+  }
+];
